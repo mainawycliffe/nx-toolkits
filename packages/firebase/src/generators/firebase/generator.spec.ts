e54ebd2cfe41deb648c42dbb43af8cc41eb7ae8d
Maina Wycliffe <wmmaina7@gmail.com>
2022-12-24 15:14:14 +0300

    chore: generate a library for firebase

---
+++ packages/firebase/src/generators/firebase/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { FirebaseGeneratorSchema } from './schema';
+
+describe('firebase generator', () => {
+  let appTree: Tree;
+  const options: FirebaseGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ packages/firebase/src/generators/firebase/schema.d.ts
@@ -0,0 +1,5 @@
+export interface FirebaseGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
