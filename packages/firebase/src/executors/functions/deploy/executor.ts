b035f6b4a9e7c131b00e15a4dcc43e7145a97906
Maina Wycliffe <wmmaina7@gmail.com>
2022-12-26 01:00:16 +0300

    feat: execute firebase deploy command on deploy

--- packages/firebase/src/executors/functions/deploy/executor.ts
+++ packages/firebase/src/executors/functions/deploy/executor.ts
@@ -1,7 +1,11 @@
+import { execSync } from 'child_process';
 import { BuildExecutorSchema } from './schema';
 
 export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for deploy', options);
+  console.log('Deploying Firebase Functions...');
+  // run firebase deploy command sync or async
+  const res = execSync('firebase deploy --only functions');
+  console.log(res.toString());
   return {
     success: true,
   };
