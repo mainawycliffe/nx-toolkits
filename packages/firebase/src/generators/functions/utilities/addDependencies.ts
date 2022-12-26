fe7beca3482efcf4a05df0c308f971e31b454d20
Maina Wycliffe <wmmaina7@gmail.com>
2022-12-26 14:17:54 +0300

    fix: correct the name of nrwl/js package to install
    #1

--- packages/firebase/src/generators/functions/utilities/addDependencies.ts
+++ packages/firebase/src/generators/functions/utilities/addDependencies.ts
@@ -1,6 +1,10 @@
 import { addDependenciesToPackageJson, Tree } from '@nrwl/devkit';
 
 export default function addDependencies(tree: Tree) {
+  // get the version of @nrwl/workspace and use it for @nrwl/js
+  const nxWorkspaceVersion = JSON.parse(tree.read('package.json').toString())
+    .devDependencies['@nrwl/workspace'];
+
   const installDependencies = addDependenciesToPackageJson(
     tree,
     {
@@ -9,7 +13,7 @@ export default function addDependencies(tree: Tree) {
     },
     {
       'firebase-functions-test': 'latest',
-      '@nrwl/js:tsc': 'latest',
+      '@nrwl/js': nxWorkspaceVersion || 'latest',
     }
   );
 
