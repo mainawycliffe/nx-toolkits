e9f4a1401ffaf9058ab2d3e7dc7c1386b8b74fe4
Maina Wycliffe <wmmaina7@gmail.com>
2022-12-31 01:41:48 +0300

    chore(firebase): remove unused executor
    Current executor runs command directly

--- packages/firebase/src/executors/functions/serve/executor.ts
+++
@@ -1,8 +0,0 @@
-import { BuildExecutorSchema } from './schema';
-
-export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for serve', options);
-  return {
-    success: true,
-  };
-}
