ad03429f29e96ffa3e7415a1b20e275c263eba78
Maina Wycliffe <wmmaina7@gmail.com>
2023-01-31 14:39:07 +0300

    chore: temp commit

--- packages/firebase/src/generators/hosting/generator.ts
+++ packages/firebase/src/generators/hosting/generator.ts
@@ -1,5 +1,5 @@
 import {
-  addProjectConfiguration,
+  updateProjectConfiguration,
   formatFiles,
   generateFiles,
   getWorkspaceLayout,
@@ -17,7 +17,10 @@ interface NormalizedSchema extends HostingGeneratorSchema {
   parsedTags: string[];
 }
 
-function normalizeOptions(tree: Tree, options: HostingGeneratorSchema): NormalizedSchema {
+function normalizeOptions(
+  tree: Tree,
+  options: HostingGeneratorSchema
+): NormalizedSchema {
   const name = names(options.name).fileName;
   const projectDirectory = options.directory
     ? `${names(options.directory).fileName}/${name}`
@@ -38,32 +41,47 @@ function normalizeOptions(tree: Tree, options: HostingGeneratorSchema): Normaliz
 }
 
 function addFiles(tree: Tree, options: NormalizedSchema) {
-    const templateOptions = {
-      ...options,
-      ...names(options.name),
-      offsetFromRoot: offsetFromRoot(options.projectRoot),
-      template: ''
-    };
-    generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
+  const templateOptions = {
+    ...options,
+    ...names(options.name),
+    offsetFromRoot: offsetFromRoot(options.projectRoot),
+    template: '',
+  };
+  generateFiles(
+    tree,
+    path.join(__dirname, 'files'),
+    options.projectRoot,
+    templateOptions
+  );
 }
 
 export default async function (tree: Tree, options: HostingGeneratorSchema) {
   const normalizedOptions = normalizeOptions(tree, options);
-  addProjectConfiguration(
-    tree,
-    normalizedOptions.projectName,
-    {
-      root: normalizedOptions.projectRoot,
-      projectType: 'library',
-      sourceRoot: `${normalizedOptions.projectRoot}/src`,
-      targets: {
-        build: {
-          executor: "@nx-toolkits/firebase:build",
-        },
+
+  // find the project we want to setup hosting for
+  // update the project configuration to add the hosting target
+  // update firebase.json, to include the hosting config
+  // NB: Firebase Hosting can support multiple sites, so we need to update the
+  // firebase.json file accordingly and make sure the user can add multiple
+  // sites to the same project
+  // For SSR, we will need Firebase Functions
+
+  updateProjectConfiguration(tree, normalizedOptions.projectName, {
+    root: normalizedOptions.projectRoot,
+    projectType: 'library',
+    sourceRoot: `${normalizedOptions.projectRoot}/src`,
+    targets: {
+      build: {
+        executor: '@nx-toolkits/firebase:build',
       },
-      tags: normalizedOptions.parsedTags,
-    }
-  );
+      deploy: {
+        executor: '@nx-toolkits/firebase:deploy',
+        // always run the build target before deploying
+        dependsOn: ['build'],
+      },
+    },
+    tags: normalizedOptions.parsedTags,
+  });
   addFiles(tree, normalizedOptions);
   await formatFiles(tree);
 }
