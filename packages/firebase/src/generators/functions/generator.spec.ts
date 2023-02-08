b831ebec37c0e964d7145d4cdcee37592341686e
Maina Wycliffe <wmmaina7@gmail.com>
2023-02-08 17:53:35 +0300

    test(firebase): add unit tests for generator
    - add unit tests for generator
    - Add Github Action to run unit tests

--- packages/firebase/src/generators/functions/generator.spec.ts
+++ packages/firebase/src/generators/functions/generator.spec.ts
@@ -6,15 +6,170 @@ import { FirebaseGeneratorSchema } from './schema';
 
 describe('firebase generator', () => {
   let appTree: Tree;
-  const options: FirebaseGeneratorSchema = { name: 'test' };
+  const options: FirebaseGeneratorSchema = {
+    name: 'test',
+    nodeVersion: '16',
+  };
 
   beforeEach(() => {
     appTree = createTreeWithEmptyWorkspace();
   });
 
-  it('should run successfully', async () => {
+  it("should throw an error if firebase project isn't defined", async () => {
+    await expect(generator(appTree, { ...options, firebaseProject: undefined }))
+      .rejects.toThrowErrorMatchingInlineSnapshot(`
+      "Firebase project has not been initialized.
+
+      Please run firebase init first or provide the firebase project id (--firebase flag) as an option to the generator.
+
+      You can find the firebase project id in the firebase console or by running firebase project:list.
+      "
+    `);
+  });
+
+  it('should run successfully if firebase project is defined', async () => {
+    await generator(appTree, { ...options, firebaseProject: 'test' });
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toMatchInlineSnapshot(`
+      Object {
+        "$schema": "../node_modules/nx/schemas/project-schema.json",
+        "name": "test",
+        "projectType": "application",
+        "root": "test",
+        "sourceRoot": "./test/src",
+        "tags": Array [],
+        "targets": Object {
+          "build": Object {
+            "configurations": Object {
+              "development": Object {
+                "minify": false,
+              },
+              "production": Object {
+                "minify": true,
+              },
+            },
+            "defaultConfiguration": "production",
+            "executor": "@nrwl/esbuild:esbuild",
+            "options": Object {
+              "assets": Array [],
+              "dependenciesFieldType": "dependencies",
+              "main": "test/src/index.ts",
+              "outputFileName": "index.js",
+              "outputPath": "dist/./test",
+              "platform": "node",
+              "project": "./test/package.json",
+              "thirdParty": false,
+              "tsConfig": "./test/tsconfig.json",
+            },
+            "outputs": Array [
+              "{options.outputPath}",
+            ],
+          },
+          "deploy": Object {
+            "command": "firebase deploy --only functions",
+          },
+          "lint": Object {
+            "executor": "@nrwl/linter:eslint",
+            "options": Object {
+              "fix": true,
+              "lintFilePatterns": Array [
+                "./test/**/*.ts",
+              ],
+            },
+            "outputs": Array [
+              "{options.outputFile}",
+            ],
+          },
+          "serve": Object {
+            "command": "firebase emulators:start --only functions",
+          },
+        },
+      }
+    `);
+  });
+
+  it('should have firebase and esbuild dependencies in package.json', async () => {
+    appTree.write(
+      '.firebaserc',
+      JSON.stringify({ projects: { default: 'test' } })
+    );
+    await generator(appTree, options);
+    const packageJson = JSON.parse(appTree.read('package.json').toString());
+
+    expect(packageJson.dependencies).toStrictEqual({
+      'firebase-admin': expect.any(String),
+      'firebase-functions': expect.any(String),
+    });
+
+    expect(packageJson.devDependencies).toStrictEqual({
+      esbuild: expect.any(String),
+      'firebase-functions-test': expect.any(String),
+      '@nrwl/esbuild': expect.any(String),
+    });
+  });
+
+  it('should run successfully if project has been initialized', async () => {
+    appTree.write(
+      '.firebaserc',
+      JSON.stringify({ projects: { default: 'test' } })
+    );
     await generator(appTree, options);
     const config = readProjectConfiguration(appTree, 'test');
-    expect(config).toBeDefined();
+    expect(config).toMatchInlineSnapshot(`
+      Object {
+        "$schema": "../node_modules/nx/schemas/project-schema.json",
+        "name": "test",
+        "projectType": "application",
+        "root": "test",
+        "sourceRoot": "./test/src",
+        "tags": Array [],
+        "targets": Object {
+          "build": Object {
+            "configurations": Object {
+              "development": Object {
+                "minify": false,
+              },
+              "production": Object {
+                "minify": true,
+              },
+            },
+            "defaultConfiguration": "production",
+            "executor": "@nrwl/esbuild:esbuild",
+            "options": Object {
+              "assets": Array [],
+              "dependenciesFieldType": "dependencies",
+              "main": "test/src/index.ts",
+              "outputFileName": "index.js",
+              "outputPath": "dist/./test",
+              "platform": "node",
+              "project": "./test/package.json",
+              "thirdParty": false,
+              "tsConfig": "./test/tsconfig.json",
+            },
+            "outputs": Array [
+              "{options.outputPath}",
+            ],
+          },
+          "deploy": Object {
+            "command": "firebase deploy --only functions",
+          },
+          "lint": Object {
+            "executor": "@nrwl/linter:eslint",
+            "options": Object {
+              "fix": true,
+              "lintFilePatterns": Array [
+                "./test/**/*.ts",
+              ],
+            },
+            "outputs": Array [
+              "{options.outputFile}",
+            ],
+          },
+          "serve": Object {
+            "command": "firebase emulators:start --only functions",
+          },
+        },
+      }
+    `);
   });
 });
