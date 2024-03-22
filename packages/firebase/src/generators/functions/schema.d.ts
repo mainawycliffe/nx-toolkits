e7aed89827a7614ba6ea683313493de85ada365d
Maina Wycliffe <wmmaina7@gmail.com>
2024-03-23 01:11:13 +0300

    chore(firebase): default to node js 18 for firebase functions

--- packages/firebase/src/generators/functions/schema.d.ts
+++ packages/firebase/src/generators/functions/schema.d.ts
@@ -1,4 +1,4 @@
-export type FirebaseNodeRuntimeVersion = '14' | '16' | '18';
+export type FirebaseNodeRuntimeVersion = '14' | '16' | '18' | '20';
 
 export interface FirebaseGeneratorSchema {
   name: string;
