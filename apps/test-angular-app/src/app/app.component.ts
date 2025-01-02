811effcb8ac2ce9d3c0837ede3f86e86820d6537
Maina Wycliffe <wmmaina7@gmail.com>
2025-01-02 21:00:33 +0300

    chore: migrate npm updates

--- apps/test-angular-app/src/app/app.component.ts
+++ apps/test-angular-app/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  standalone: true,
-  imports: [],
-  selector: 'nx-toolkits-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    imports: [],
+    selector: 'nx-toolkits-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {}
